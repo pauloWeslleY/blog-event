b4c306f61bea339766217a0e096f15f19729c5d3
pauloWeslleY <weslleyspfc13@outlook.com>
2024-09-28 15:15:09 -0300

    refactor: change usecases Authentication

--- src/domain/authentication/usecases/remote-authentication.ts
+++ src/domain/authentication/usecases/remote-authentication.ts
@@ -1,13 +1,14 @@
-import { IAuth } from '@/infra/firebase'
+import { UserCredential } from 'firebase/auth'
+import { ISignIn } from '@/infra/firebase'
 import { IAuthentication } from '@/data/usecases'
 import { authenticationAdapter } from '../adapter/authentication-adapter'
 
 interface RemoteAuthenticationDependencies {
-  readonly auth: IAuth.SignInAuth
+  readonly auth: ISignIn<UserCredential>
 }
 
 export class RemoteAuthentication implements IAuthentication {
-  private auth: IAuth.SignInAuth
+  private auth: ISignIn<UserCredential>
 
   constructor(
     protected readonly dependencies: RemoteAuthenticationDependencies,
@@ -18,7 +19,7 @@ export class RemoteAuthentication implements IAuthentication {
   async authentication(
     params: IAuthentication.Params,
   ): Promise<RemoteAuthentication.Model> {
-    const { credential, error } = await this.auth.authSignIn(params)
+    const { credential, error } = await this.auth.signIn(params)
     const accessToken = await credential?.user.getIdToken()
 
     return {
